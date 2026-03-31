e5b5d5db6f79735821fdc70c6cc257d01c490d38
Atulkumar58 <atulchaudhary151@gmail.com>
2026-04-01 02:51:26 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score= "3abc"
 
-console.log(typeof score); // number
-console.log(typeof Number(score + "")); // string
-console.log (Number(score));
+// console.log(typeof score); // number
+// console.log(typeof Number(score + "")); // string
+// console.log (Number(score));
 
 //typecasting
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 //type casting
 //"33" => 33
@@ -15,3 +15,15 @@ console.log(typeof valueInNumber);
 // false => 0
 // null => 0
 // undefined => NaN
+
+let val= 3
+let neg = -val
+// console.log(neg); // -3 
+
+// console.log(2**2); //power operator
+
+let str= "hello";
+let str2= "world";
+// console.log(str + " " + str2); // string concatenation
+
+
