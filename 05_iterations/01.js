88b6a3a8a82c0d83212ee949237f17fe6bddcfc6
Atulkumar58 <atulchaudhary151@gmail.com>
2026-06-01 13:10:47 +0530

    Add examples of for loop and for of loop in 01.js and 03.js

---
+++ 05_iterations/01.js
@@ -0,0 +1,12 @@
+//for
+for(let i = 0; i < 5; i++){
+    console.log(i) // Output: 0, 1, 2, 3, 4
+}
+
+let myArray = ['apple', 'banana', 'cherry'];
+for(let i =0; i < myArray.length; i++){
+    console.log(myArray[i]) // Output: apple, banana, cherry
+}
+
+// continue
+// break
